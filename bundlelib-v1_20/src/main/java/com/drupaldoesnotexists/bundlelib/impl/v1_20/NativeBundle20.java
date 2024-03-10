0a18cfd766357c87718571811fcb1489b9f1b2f3
Dmitry Lomov <drupal.doesnot.exists@mail.ru>
2024-03-10 15:30:44 +0300

    feat: 1.20.x support

---
+++ bundlelib-v1_20/src/main/java/com/drupaldoesnotexists/bundlelib/impl/v1_20/NativeBundle20.java
@@ -0,0 +1,27 @@
+package com.drupaldoesnotexists.bundlelib.impl.v1_20;
+
+import com.drupaldoesnotexists.bundlelib.adapter.Bundle;
+import net.minecraft.network.protocol.BundlePacket;
+import net.minecraft.network.protocol.Packet;
+import net.minecraft.network.protocol.game.ClientGamePacketListener;
+import org.jetbrains.annotations.NotNull;
+
+public class NativeBundle20 implements Bundle<Packet<ClientGamePacketListener>> {
+
+    private final BundlePacket<ClientGamePacketListener> bundlePacket;
+
+    public NativeBundle20(BundlePacket<ClientGamePacketListener> packet) {
+        this.bundlePacket = packet;
+    }
+
+    @Override
+    public @NotNull Iterable<Packet<ClientGamePacketListener>> getBundledPackets() {
+        return bundlePacket.subPackets();
+    }
+
+    @Override
+    public @NotNull Packet<ClientGamePacketListener> asPacket() {
+        return bundlePacket;
+    }
+
+}
